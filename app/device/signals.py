9fd6e14d3a234e0c0fbd7e4eeb360983f4c4d200
oyekamal <autocratickamal1@gmail.com>
2024-02-14 10:57:34 +0500

    updated signals

--- app/device/signals.py
+++ app/device/signals.py
@@ -1,7 +1,7 @@
 # your_app/signals.py
 # import paho.mqtt.client as mqtt
 from device.mqtt_functions import client
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from .models import Relay, RelayGroup, Device
@@ -12,7 +12,6 @@ def update_relays_on_relaygroup_change(sender, instance, **kwargs):
     if kwargs.get("created", False):
         # The RelayGroup instance is being created, no need to update relays.
         return
-
     print(f"Updating relays for RelayGroup: {instance}")
 
     # Get the related Relay instances
@@ -34,28 +33,35 @@ def update_relays(sender, instance, **kwargs):
         each_relay.save()
 
 
-@receiver(post_save, sender=Relay)
+@receiver(pre_save, sender=Relay)
 def update_relays(sender, instance, **kwargs):
     print("signals of relay")
-    if kwargs.get("created", False):
-        # The RelayGroup instance is being created, no need to update relays.
-        return
-
-    device_info = {
-        "device_type": "slave",
-        "device_name": "",
-        "extra_info": "",
-        "ip": "",
-        "port": "",
-        "RELAY_PINS": {},
-        "relay_on_off": {},
-        "message": "Update relay",
-        "device_update": False,
-    }
-    device_info["device_name"] = instance.device.device_name
-    device_info["ip"] = instance.device.device_ip
-    device_info["device_update"] = True
-    device_info["relay_on_off"][instance.relay_pin] = instance.is_on
-    device = device_info["device_name"]+":" + device_info['ip']
-    result = client.publish(device, str(device_info))
-    print(f"Updating relays: {instance}")
+    # Check if the instance is being updated (not created)
+    if not kwargs.get('created', False):
+        # Get the original instance from the database
+        orig = Relay.objects.filter(pk=instance.pk)
+        if orig:
+            orig = orig[0]
+            print(orig.is_on,  "   !=   ", instance.is_on)
+            # Compare the original is_on value with the current instance's is_on value
+            if orig.is_on != instance.is_on:
+                # The is_on field has changed, proceed with your logic
+                print("is_on has changed, updating")
+                device_info = {
+                    "device_type": "slave",
+                    "device_name": "",
+                    "extra_info": "",
+                    "ip": "",
+                    "port": "",
+                    "RELAY_PINS": {},
+                    "relay_on_off": {},
+                    "message": "Update relay",
+                    "device_update": False,
+                }
+                device_info["device_name"] = instance.device.device_name
+                device_info["ip"] = instance.device.device_ip
+                device_info["device_update"] = True
+                device_info["relay_on_off"][instance.relay_pin] = instance.is_on
+                device = device_info["device_name"]+":" + device_info['ip']
+                result = client.publish(device, str(device_info))
+                print(f"Updating relays: {instance}")
